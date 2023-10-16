7c713d3b96bf45269c6b59648017d1db778b79a0
Mário Luiz  Guimarães <mariolsg.oficial@gmail.com>
2023-10-16 20:06:21 -0300

    .

---
+++ PPI - JavaScript/Atividade-1/script.js
@@ -0,0 +1,26 @@
+document.getElementById('cadastroForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+    let confirmaSenha = document.getElementById('confirmaSenha').value;
+
+    let mensagem = document.getElementById('mensagem');
+
+    if (nome && email && senha && senha === confirmaSenha) {
+        mensagem.className = 'mensagem-sucesso';
+        mensagem.innerText = 'Você foi cadastrado com sucesso!';
+        window. location="cadastrado.html"
+        document.getElementById("nome_user").innerText = nome;
+
+    } else {
+        mensagem.className = 'mensagem-erro';
+        mensagem.innerText = 'Por favor, preencha todos os campos corretamente.';
+    }
+});
+
+document.getElementById('cadastroForm').addEventListener('reset', function() {
+    let mensagem = document.getElementById('mensagem');
+    mensagem.className = 'hidden';
+});
