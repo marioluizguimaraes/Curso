8e39f4b0072d2d6aa38b72c1172fcdfa44bb93fb
Mário Luiz  Guimarães <mariolsg.oficial@gmail.com>
2023-10-20 09:22:37 -0300

    .

--- PPI - JavaScript/Atividade-1/Assunto.js
+++
@@ -1,4 +0,0 @@
-/* 
-SINTAXE: JavaScript é uma linguagem de programação que segue uma sintaxe específica. Ela utiliza palavras-chave, operadores e convenções de escrita para instruir o computador a executar ações.
-*/
-
