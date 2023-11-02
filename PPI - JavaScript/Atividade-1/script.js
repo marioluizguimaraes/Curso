12bf0c1eba1c7dcf48c4834da518e537ef847f8f
Mário Luiz  Guimarães <mariolsg.oficial@gmail.com>
2023-11-02 09:06:46 -0300

    .

--- PPI - JavaScript/Atividade-1/script.js
+++ PPI - JavaScript/Atividade-1/script.js
@@ -1,25 +1,25 @@
 document.getElementById('cadastroForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    let nome            = document.getElementById('nome').value;
-    let email           = document.getElementById('email').value;
-    let senha           = document.getElementById('senha').value;
-    let confirmaSenha   = document.getElementById('confirmaSenha').value;
+    let nome                = document.getElementById('nome').value;
+    let email               = document.getElementById('email').value;
+    let senha               = document.getElementById('senha').value;
+    let confirmaSenha       = document.getElementById('confirmaSenha').value;
 
-    let mensagem        = document.getElementById('mensagem');
+    let mensagem            = document.getElementById('mensagem');
 
     if (nome && email && senha && senha === confirmaSenha) {
-        mensagem.className = 'mensagem-sucesso';
-        mensagem.innerText = 'Você foi cadastrado com sucesso!';
+        mensagem.className  = 'mensagem-sucesso';
+        mensagem.innerText  = 'Você foi cadastrado com sucesso!';
 
     } else {
-        mensagem.className = 'mensagem-erro';
-        mensagem.innerText = 'Por favor, preencha todos os campos corretamente.';
+        mensagem.className  = 'mensagem-erro';
+        mensagem.innerText  = 'Por favor, preencha todos os campos corretamente.';
     }
 });
 
 document.getElementById('cadastroForm').addEventListener('reset', function() {
-    let mensagem = document.getElementById('mensagem');
-    mensagem.className = 'hidden';
+    let mensagem            = document.getElementById('mensagem');
+    mensagem.className      = 'hidden';
 });
 
